72723c1951c076dda36246cfeb55e09dc5437e42
Philip McMahon <philip.mcmahon@guardian.co.uk>
2022-11-30 18:29:11 +0000

    Build guardian fork of securedrop. Auto publish to guardian apt repo

---
+++ guardian-release/publish-securedrop.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+
+VERSION=$1
+DEB_LOCATION=$2
+
+SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
+
+print_usage_and_exit () {
+  cat << EOF
+Usage: SIGNING_KEY_SECRET_ID=<id> publish-securedrop.sh VERSION DEB_LOCATION
+
+e.g. SIGNING_KEY_SECRET_ID=my-secret ./publish-securedrop.sh 100.8.1 securedrop.deb
+EOF
+  exit 1
+}
+
+if [ -z "$VERSION" ]; then
+    echo "Version must be provided"
+    print_usage_and_exit
+    exit 1
+fi
+
+if [ -z "$DEB_LOCATION" ]; then
+    echo "Fully qualified location of the deb file you want to publish should be provided"
+    print_usage_and_exit
+    exit 1
+fi
+
+if [ -z "$SIGNING_KEY_SECRET_ID" ]; then
+  echo "$SIGNING_KEY_SECRET_ID environment variable must be set"
+  print_usage_and_exit
+fi
+
+# Fetch signing key
+aws secretsmanager get-secret-value --region eu-west-1 --secret-id "$SIGNING_KEY_SECRET_ID" | jq .SecretString -r > /tmp/private.asc
+
+REPO_NAME="gu-securedrop"
+SNAPSHOT_NAME="$REPO_NAME-$VERSION"
+KEYRING="temp-keyring.gpg"
+# Aptly is possibly a bit more full featured than we need - it allows a local version of a repository
+# Rather than keep this in sync between different developer machines, here we drop everything local before publishing
+
+# Remove any local aptly stuff - || true is there because we don't want this script to fail if there's not existing stuff
+aptly repo drop -force "$REPO_NAME" || true
+aptly publish drop bullseye s3:s3-endpoint: || true
+aptly snapshot drop "$SNAPSHOT_NAME" || true
+
+# Import key into temporary keyring
+gpg --no-default-keyring --keyring "$KEYRING" --fingerprint
+gpg --no-default-keyring  --keyring "$KEYRING" --import /tmp/private.asc
+
+# Publish debs to S3
+aptly repo create -distribution=bullseye -component=main "$REPO_NAME"
+aptly repo add "$REPO_NAME" "$DEB_LOCATION"
+aptly snapshot create "$SNAPSHOT_NAME" from repo "$REPO_NAME"
+aptly publish snapshot -config=$SCRIPT_PATH/aptly.conf -keyring="$KEYRING" "$SNAPSHOT_NAME" s3:s3-endpoint:
+
+# Remove temporary keyring
+rm ~/.gnupg/temp-keyring.gpg
+rm /tmp/private.asc

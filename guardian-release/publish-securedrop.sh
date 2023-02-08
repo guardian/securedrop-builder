9d87c496adbd7ed38da93d57e81f7808a9260cb1
Philip McMahon <philip.mcmahon@guardian.co.uk>
2023-02-08 11:41:07 +0000

    Improve documentation. Remove aptly config file - now preinstalled on build server

--- guardian-release/publish-securedrop.sh
+++ guardian-release/publish-securedrop.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+# This script is designed to be run on a debain build server. It handles the process of signing a .deb package and publishing
+# it to the guardian's APT repository in amazon S3. Where the signing key is encrypted there will be a prompt for the password
 set -e
 
 VERSION=$1
 DEB_LOCATION=$2
+SIGNING_KEY_SECRET_ID=$3
 
 SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
 
 print_usage_and_exit () {
   cat << EOF
-Usage: SIGNING_KEY_SECRET_ID=<id> publish-securedrop.sh VERSION DEB_LOCATION
+Usage: publish-securedrop.sh VERSION DEB_LOCATION
 
-e.g. SIGNING_KEY_SECRET_ID=my-secret ./publish-securedrop.sh 100.8.1 securedrop.deb
+e.g. ./publish-securedrop.sh 100.8.1 securedrop.deb mysecret-id
 EOF
   exit 1
 }
@@ -28,7 +31,8 @@ if [ -z "$DEB_LOCATION" ]; then
 fi
 
 if [ -z "$SIGNING_KEY_SECRET_ID" ]; then
-  echo "$SIGNING_KEY_SECRET_ID environment variable must be set"
+  echo "You must provide the secret name for the signing key."
+   echo "(If calling this via make securedrop-client you may need to set SIGNING_KEY_SECRET_ID)"
   print_usage_and_exit
 fi
 
@@ -39,16 +43,18 @@ KEYRING="temp-keyring.gpg"
 # Aptly is possibly a bit more full featured than we need - it allows a local version of a repository
 # Rather than keep this in sync between different developer machines, here we drop everything local before publishing
 
+# Note - aptly needs a configuration file in order to e.g. know which s3 bucket to publish to. This should be preloaded
+# onto the server automatically in /home/admin/.aptly.conf
+
 # Remove any local aptly stuff - || true is there because we don't want this script to fail if there's not existing stuff
 aptly repo drop -force "$REPO_NAME" || true
-aptly publish drop bullseye s3:s3-endpoint: || true
+aptly publish drop bullseye s3:guardian-securedrop-repo: || true
 aptly snapshot drop "$SNAPSHOT_NAME" || true
 
 # Fetch signing key
 aws secretsmanager get-secret-value --region eu-west-1 --secret-id "$SIGNING_KEY_SECRET_ID" | jq .SecretString -r > /home/admin/private.asc
 
 # Import key into temporary keyring
-gpg --no-default-keyring --keyring "$KEYRING" --fingerprint
 gpg --no-default-keyring --pinentry loopback --keyring "$KEYRING" --import /home/admin/private.asc
 
 rm /home/admin/private.asc
@@ -57,7 +63,8 @@ rm /home/admin/private.asc
 aptly repo create -distribution=bullseye -component=main "$REPO_NAME"
 aptly repo add "$REPO_NAME" "$DEB_LOCATION"
 aptly snapshot create "$SNAPSHOT_NAME" from repo "$REPO_NAME"
-aptly publish snapshot -config=$SCRIPT_PATH/aptly.conf -keyring="$KEYRING" "$SNAPSHOT_NAME" s3:s3-endpoint:
+
+aptly publish snapshot -keyring="$KEYRING" "$SNAPSHOT_NAME" s3:guardian-securedrop-repo:
 
 # Remove temporary keyring
-rm ~/.gnupg/temp-keyring.gpg
+rm ~/.gnupg/$KEYRING
